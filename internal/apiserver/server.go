548146ed3e9c661115c4796307d0ac642eb47741
rowasjo <76393157+rowasjo@users.noreply.github.com>
2024-08-05 23:07:33 +0200

    initial openapi validation

--- internal/apiserver/server.go
+++ internal/apiserver/server.go
@@ -1,8 +1,12 @@
 package apiserver
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3filter"
+	"github.com/getkin/kin-openapi/routers/gorillamux"
 	"github.com/rowasjo/tinyvalgo/assets"
 )
 
@@ -34,9 +38,43 @@ func docsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getBlobHandler(w http.ResponseWriter, r *http.Request) {
+	err := validateRequest(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	http.NotFound(w, r)
 }
 
 func putBlobHandler(w http.ResponseWriter, r *http.Request) {
+	err := validateRequest(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	http.NotFound(w, r)
+}
+
+func validateRequest(r *http.Request) error {
+	// TODO: parse schema only once, refactor
+
+	ctx := context.Background()
+	loader := &openapi3.Loader{Context: ctx, IsExternalRefsAllowed: true}
+	doc, err := loader.LoadFromData(assets.OpenapiYaml)
+	if err != nil {
+		panic(err)
+	}
+
+	router, _ := gorillamux.NewRouter(doc)
+
+	route, pathParams, _ := router.FindRoute(r)
+
+	requestValidationInput := &openapi3filter.RequestValidationInput{
+		Request:    r,
+		PathParams: pathParams,
+		Route:      route,
+	}
+	return openapi3filter.ValidateRequest(ctx, requestValidationInput)
 }
