bb6d7aaa9f54cadb5777f8728bf1d94206d183b7
rowasjo <76393157+rowasjo@users.noreply.github.com>
2024-10-20 12:05:53 +0200

    shutdown handling

--- cmd/tinyvalapi/main.go
+++ cmd/tinyvalapi/main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
 
 	"github.com/rowasjo/tinyvalgo/internal/tinyvalapi"
 )
 
-func main() {
-	handler := tinyvalapi.NewServer()
+func run(ctx context.Context, w io.Writer, args []string) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
 
-	err := http.ListenAndServe(":8080", handler)
-	if err != nil {
-		log.Fatal(err)
+	srv := tinyvalapi.NewServer()
+
+	httpServer := &http.Server{
+		Addr:    net.JoinHostPort("", "8080"),
+		Handler: srv,
+	}
+
+	go func() {
+		log.Printf("listening on %s\n", httpServer.Addr)
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(os.Stderr, "error listening and serving: %s\n", err)
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		shutdownCtx := context.Background()
+		shutdownCtx, cancel := context.WithTimeout(shutdownCtx, 10*time.Second)
+		defer cancel()
+		if err := httpServer.Shutdown(shutdownCtx); err != nil {
+			fmt.Fprintf(os.Stderr, "error shutting down http server: %s\n", err)
+		}
+	}()
+	wg.Wait()
+	return nil
+}
+
+func main() {
+
+	ctx := context.Background()
+	if err := run(ctx, os.Stdout, os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
 }
